df2f171878e009fa489a75f7d6db0529538562d0
Samuel <samuel.young@softwire.com>
2024-08-16 16:12:08 +0000

    add material

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </StrictMode>,
 )
