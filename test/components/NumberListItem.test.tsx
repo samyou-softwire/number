2122270dd40423dc05c601907b8d11985eeb079f
Samuel <samuel.young@softwire.com>
2024-09-27 15:42:22 +0000

    prettify tests

--- test/components/NumberListItem.test.tsx
+++ test/components/NumberListItem.test.tsx
@@ -1,57 +1,59 @@
-import { describe, expect, it, vi } from "vitest"
-import { render, screen } from "@testing-library/react"
-import NumberListItem, { NumberListItemProps } from "../../src/components/NumberListItem"
-import React from "react"
-import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import NumberListItem, {
+  NumberListItemProps,
+} from "../../src/components/NumberListItem";
+import React from "react";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
 const renderComponent = (props: Partial<NumberListItemProps> = {}) => {
-    const defaultProps: NumberListItemProps = {
-        number: 10
-    }
+  const defaultProps: NumberListItemProps = {
+    number: 10,
+  };
 
-    return render(<NumberListItem {...defaultProps} {...props}/>);
-}
+  return render(<NumberListItem {...defaultProps} {...props} />);
+};
 
 describe("NumberListItem", () => {
-    it("renders", () => {
-        vi.mock("@tanstack/react-query");
+  it("renders", () => {
+    vi.mock("@tanstack/react-query");
 
-        vi.mocked(useQuery).mockReturnValue({
-            data: "10 is a cool number",
-            error: false,
-            fetchStatus: "idle"
-        } as unknown as UseQueryResult<unknown, unknown>);
+    vi.mocked(useQuery).mockReturnValue({
+      data: "10 is a cool number",
+      error: false,
+      fetchStatus: "idle",
+    } as unknown as UseQueryResult<unknown, unknown>);
 
-        renderComponent();
+    renderComponent();
 
-        expect(() => screen.getByText("10 is a cool number")).not.toThrowError()
-    });
+    expect(() => screen.getByText("10 is a cool number")).not.toThrowError();
+  });
 
-    it("shows an error", () => {
-        vi.mock("@tanstack/react-query");
+  it("shows an error", () => {
+    vi.mock("@tanstack/react-query");
 
-        vi.mocked(useQuery).mockReturnValue({
-            data: "10 is a cool number",
-            error: true,
-            fetchStatus: "idle"
-        } as unknown as UseQueryResult<unknown, unknown>);
+    vi.mocked(useQuery).mockReturnValue({
+      data: "10 is a cool number",
+      error: true,
+      fetchStatus: "idle",
+    } as unknown as UseQueryResult<unknown, unknown>);
 
-        renderComponent();
+    renderComponent();
 
-        expect(() => screen.getByText("error")).not.toThrowError();
-    });
+    expect(() => screen.getByText("error")).not.toThrowError();
+  });
 
-    it("shows a skeleton", () => {
-        vi.mock("@tanstack/react-query");
+  it("shows a skeleton", () => {
+    vi.mock("@tanstack/react-query");
 
-        vi.mocked(useQuery).mockReturnValue({
-            data: "10 is a cool number",
-            error: false,
-            fetchStatus: "fetching"
-        } as unknown as UseQueryResult<unknown, unknown>);
+    vi.mocked(useQuery).mockReturnValue({
+      data: "10 is a cool number",
+      error: false,
+      fetchStatus: "fetching",
+    } as unknown as UseQueryResult<unknown, unknown>);
 
-        renderComponent();
+    renderComponent();
 
-        expect(() => screen.getByText("error")).not.toThrowError();
-    });
-})
+    expect(() => screen.getByText("error")).not.toThrowError();
+  });
+});
--- test/uat/App.test.ts
+++ test/uat/App.test.ts
@@ -1,37 +1,37 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { Browser, Builder, By, WebDriver } from "selenium-webdriver"
+import { Browser, Builder, By, WebDriver } from "selenium-webdriver";
 
 describe("app", () => {
-    let driver: WebDriver;
+  let driver: WebDriver;
 
-    beforeAll(async () => {
-        driver = await new Builder().forBrowser(Browser.CHROME).build();
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
 
-        await driver.manage().setTimeouts({ implicit: 2000 });
-    });
+    await driver.manage().setTimeouts({ implicit: 2000 });
+  });
 
-    it('loads', async () => {
-        await driver.get('http://localhost:5173/');
+  it("loads", async () => {
+    await driver.get("http://localhost:5173/");
 
-        const title = await driver.getTitle();
+    const title = await driver.getTitle();
 
-        expect(title).to.equal("Random Numbers");
-    });
+    expect(title).to.equal("Random Numbers");
+  });
 
-    it('shows some random facts', async () => {
-        await driver.get('http://localhost:5173/');
+  it("shows some random facts", async () => {
+    await driver.get("http://localhost:5173/");
 
-        const addButtonField = await driver.findElement(By.id('add-number'));
-        await addButtonField.sendKeys('5');
-        
-        const submitButton = await driver.findElement(By.id('add-button'));
-        await submitButton.click();
+    const addButtonField = await driver.findElement(By.id("add-number"));
+    await addButtonField.sendKeys("5");
 
-        const numberSubtitle = await driver.findElement(By.id('number-subtitle-5'));
-        expect(await numberSubtitle.getText()).to.equal('5');
-    });
+    const submitButton = await driver.findElement(By.id("add-button"));
+    await submitButton.click();
 
-    afterAll(async () => {
-        await driver.close();
-    });
-})
+    const numberSubtitle = await driver.findElement(By.id("number-subtitle-5"));
+    expect(await numberSubtitle.getText()).to.equal("5");
+  });
+
+  afterAll(async () => {
+    await driver.close();
+  });
+});
