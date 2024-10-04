23890aa1b00e7ac32cc6df48dc92a001622fb1fd
Samuel <samuel.young@softwire.com>
2024-10-04 13:09:35 +0000

    add cucumber example

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('today is Sunday', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I ask whether it\'s Friday yet', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should be told {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
